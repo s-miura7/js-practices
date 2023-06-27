3a0e2a1360465f0f5a67824a53549f4cccaaad70
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-27 09:35:37 +0900

    変数名をキャメルケースに変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,15 +1,15 @@
 var argv = require("minimist")(process.argv.slice(2));
-const current_date = new Date();
-const current_month = argv.m ? argv.m : current_date.getMonth() + 1;
-const current_year = argv.y ? argv.y : current_date.getFullYear();
-const last_date = new Date(current_year, current_month, 0).getDate();
-const wday = new Date(current_year, current_month - 1, 1).getDay();
+const currentDate = new Date();
+const currentMonth = argv.m ? argv.m : currentDate.getMonth() + 1;
+const currentYear = argv.y ? argv.y : currentDate.getFullYear();
+const lastDate = new Date(currentYear, currentMonth, 0).getDate();
+const wday = new Date(currentYear, currentMonth - 1, 1).getDay();
 const wdays = ["日", "月", "火", "水", "木", "金", "土", "\n"];
 
 wdays.forEach((wday) => process.stdout.write(wday.padStart(2, " ")));
-console.log(`${current_month}月 ${current_year}`.padStart(13, " "));
+console.log(`${currentMonth}月 ${currentYear}`.padStart(13, " "));
 process.stdout.write(" ".repeat(3 * wday));
-for (let date = 1; date <= last_date; date++) {
+for (let date = 1; date <= lastDate; date++) {
   process.stdout.write(String(date).padStart(3, " "));
   if ((date + wday) % 7 == 0) {
     console.log("\n");
