c5ca7161a398dc53084504056d0864e81d0cbaea
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-27 09:38:13 +0900

    変数名につけたcurrentを削除

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,13 +1,13 @@
 var argv = require("minimist")(process.argv.slice(2));
-const currentDate = new Date();
-const currentMonth = argv.m ? argv.m : currentDate.getMonth() + 1;
-const currentYear = argv.y ? argv.y : currentDate.getFullYear();
-const lastDate = new Date(currentYear, currentMonth, 0).getDate();
-const wday = new Date(currentYear, currentMonth - 1, 1).getDay();
+const date = new Date();
+const month = argv.m ? argv.m : date.getMonth() + 1;
+const year = argv.y ? argv.y : date.getFullYear();
+const lastDate = new Date(year, month, 0).getDate();
+const wday = new Date(year, month - 1, 1).getDay();
 const wdays = ["日", "月", "火", "水", "木", "金", "土", "\n"];
 
 wdays.forEach((wday) => process.stdout.write(wday.padStart(2, " ")));
-console.log(`${currentMonth}月 ${currentYear}`.padStart(13, " "));
+console.log(`${month}月 ${year}`.padStart(13, " "));
 process.stdout.write(" ".repeat(3 * wday));
 for (let date = 1; date <= lastDate; date++) {
   process.stdout.write(String(date).padStart(3, " "));
